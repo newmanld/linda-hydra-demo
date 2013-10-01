73ac6b0ce92d90a19fc8d735be4b01012a1fbf28
Linda Newman <newmanld@ucmail.uc.edu>
2013-10-01 15:32:05 -0400

    Ran hydra generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HydraDemoCwr::Application.config.secret_key_base = 'b3be25a1f92bb9e34df1bb3f8ad6596e7ee5f09312dcf453d6b5d09e652e1ac0bbf344733dbbc402d5c5a684fa66852e79dd3ca5eab4e2a5912176a50e978fc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HydraDemoCwr::Application.config.session_store :cookie_store, key: '_hydra-demo-cwr_session'
